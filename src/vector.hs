2ce918fff943f247be712c4ccfa0b061cabe1cb3
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-07-07 18:36:26 +0200

    added basic vector functions

---
+++ src/vector.hs
@@ -0,0 +1,17 @@
+module Vector (
+	Vector(..),
+	add,
+	mul,
+	distance
+) where
+ 
+data Vector = Vector Double Double deriving (Show, Eq)
+
+add :: Vector -> Vector -> Vector
+add (Vector x1 y1) (Vector x2 y2) = Vector (x1 + x2) (y1 + y2);
+
+mul :: Double -> Vector -> Vector
+mul factor (Vector x y) = Vector (factor * x) (factor * y)
+
+distance :: Vector -> Vector -> Double
+distance (Vector x1 y1) (Vector x2 y2) = sqrt $ (x1 + x2) ^ 2 + (y1 + y2) ^ 2
